022a8d69d7a771e00c65b3f5c46645f92aa3e76b
Lê Bảo Long <longle12042006a@gmail.com>
2023-10-20 22:26:35 +0700

    upload

--- @types/model-viewer.d.ts
+++ @types/model-viewer.d.ts
@@ -1,7 +1,41 @@
 declare module '@google/model-viewer' {
-    namespace ModelViewerElement {
-      export interface ModelViewerAttributes {
-        ar?: boolean;
-      }
+  namespace ModelViewerElement {
+    export interface ModelViewerAttributes {
+      ar?: boolean;
     }
   }
+}
+
+export declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": React.DetailedHTMLProps<
+        React.AllHTMLAttributes<
+          Partial<globalThis.HTMLElementTagNameMap['model-viewer']>
+        >,
+        Partial<globalThis.HTMLElementTagNameMap['model-viewer']>
+      >;
+    }
+  }
+}
+
+interface ModelViewerProps extends React.HTMLProps<HTMLDivElement> {
+  id: string;
+  "ar": true;
+  "ar-scale": string;
+  "camera-controls": true;
+  "touch-action": string;
+  "auto-rotate": true;
+  "src": string;
+  "shadow-intensity": string;
+  "alt": string;
+  "title": string;
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'model-viewer': ModelViewerProps;
+    }
+  }
+}
