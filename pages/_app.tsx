07be52da240c335195310806d06e6eb535e7e9a2
Lê Bảo Long <longle12042006a@gmail.com>
2023-10-28 11:57:20 +0700

    upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,7 +34,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   setupPostHog();
   return (
     <>
-    
       <AdvancedScripts/>
       <DefaultSeo {...SEO}/>
       <Component {...pageProps} />
