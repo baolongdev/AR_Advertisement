7b03368288700361b4c666d87e63824ef89fa45b
Lê Bảo Long <longle12042006a@gmail.com>
2023-12-02 11:27:23 +0700

    add 👋 Activate AR 👉

--- components/model/model-viewer.tsx
+++ components/model/model-viewer.tsx
@@ -1,16 +1,31 @@
-import React, { HTMLAttributes, useEffect, useRef, useState } from 'react';
-import dimensions from './dimensions';
-import { Switch, Text } from '@tremor/react';
-import { motion } from 'framer-motion';
-import PluginControls from './plugin';
+import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
+import dimensions from "./dimensions";
+import { Switch, Text } from "@tremor/react";
+import { motion } from "framer-motion";
+import PluginControls from "./plugin";
 
-export default function ModelViewer({ linkUrl, title = "", description = "",...props }) {
+export default function ModelViewer({
+  linkUrl,
+  title = "",
+  description = "",
+  ...props
+}) {
   const [dimensionsOn, setDimensionsOn] = useState<boolean>(false);
   const [animationOn, setAnimationOn] = useState<boolean>(false);
-
+  const [platform, setPlatform] = useState<boolean>(false);
   useEffect(() => {
-    dimensions(dimensionsOn)    
-  }, [dimensionsOn])
+    dimensions(dimensionsOn);
+  }, [dimensionsOn]);
+  useEffect(() => {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      // console.log("abc");
+      setPlatform(true);
+    }
+  }, []);
 
   return (
     <model-viewer
@@ -20,7 +35,6 @@ export default function ModelViewer({ linkUrl, title = "", description = "",...p
       title={title}
       alt={description}
       {...(animationOn ? { autoplay: true } : {})}
-
       ar-scale="auto"
       camera-controls
       touch-action="pan-y"
@@ -33,19 +47,47 @@ export default function ModelViewer({ linkUrl, title = "", description = "",...p
         drag
         dragConstraints={{ left: 0, right: 170, top: 150, bottom: 300 }}
         // className='hidden sm:block'
-
       >
         <div className="flex flex-col gap-3 absolute top-3 right-3 w-52 p-2 rounded-md bg-white">
-          <div className='w-full flex gap-2'>
+          <div className="w-full flex gap-2">
             <Text>Hiện kích thức</Text>
-            <Switch id="switchDimensions" name="switch" checked={dimensionsOn} onChange={() => { setDimensionsOn(!dimensionsOn) }} />
+            <Switch
+              id="switchDimensions"
+              name="switch"
+              checked={dimensionsOn}
+              onChange={() => {
+                setDimensionsOn(!dimensionsOn);
+              }}
+            />
           </div>
-          <div className='w-full flex gap-2'>
+          <div className="w-full flex gap-2">
             <Text>Animation</Text>
-            <Switch id="switchAnimation" name="switch" checked={animationOn} onChange={() => { setAnimationOn(!animationOn) }} />
+            <Switch
+              id="switchAnimation"
+              name="switch"
+              checked={animationOn}
+              onChange={() => {
+                setAnimationOn(!animationOn);
+              }}
+            />
           </div>
         </div>
       </motion.div>
+      <div
+        className={`
+          absolute
+          bottom-4
+          right-16
+          transform-[]
+          p-2
+          bg-white
+          text-black
+          rounded-l-xl
+          ${platform ? "" : "hidden"}
+        `}
+      >
+        <p className="select-none">👋 Activate AR 👉</p>
+      </div>
     </model-viewer>
   );
 }
