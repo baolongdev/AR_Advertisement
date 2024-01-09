c471f559b231a178e2bf4c3d9339cb7dcab4b873
Lê Bảo Long <longle12042006a@gmail.com>
2024-01-09 22:29:27 +0700

    update SpeedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,40 @@
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { DefaultSeo } from 'next-seo'
-import SEO from '../next-seo.config';
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-import posthog from 'posthog-js';
-import { Analytics } from '@vercel/analytics/react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import AdvancedScripts from '../components/utils/advanced-scripts';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import posthog from "posthog-js";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AdvancedScripts from "../components/utils/advanced-scripts";
 
 function setupPostHog() {
   // setup posthog
   const router = useRouter();
   useEffect(() => {
     // Init PostHog
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, { api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST });
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    });
 
     // Track page views
-    const handleRouteChange = () => posthog.capture('$pageview');
-    router.events.on('routeChangeComplete', handleRouteChange);
+    const handleRouteChange = () => posthog.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
 
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, []);
-
 }
 export default function MyApp({ Component, pageProps }: AppProps) {
   setupPostHog();
   return (
     <>
-      <AdvancedScripts/>
-      <DefaultSeo {...SEO}/>
+      <AdvancedScripts />
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
       <Analytics />
       <ToastContainer
@@ -49,5 +50,5 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         theme="light"
       />
     </>
-  )
+  );
 }
